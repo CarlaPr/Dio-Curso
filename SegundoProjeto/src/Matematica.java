eec5993ab23b115204f1020bf3853b8ca9c97d9e
carlapr <carlaperola.823@outlook.com>
2023-09-16 11:06:27 -0300

    salvando

--- SegundoProjeto/src/Matematica.java
+++ SegundoProjeto/src/Matematica.java
@@ -1,21 +1,6 @@
-import java.util.Scanner;
 public class Matematica {
 
     public static void main (String [] args ) {
         
-        System.out.println("Idade da Turma");
-        Scanner m = new Scanner(System.in);
-        int idade , maiorIdade = 0;
-
-        for (int i = 0; i<4; i++){
-            System.out.print("Digite sua idade: ");
-            idade = m.nextInt();
-            
-            if (idade>maiorIdade){
-                maiorIdade = idade;
-            }
-        }
-        System.out.println(maiorIdade);
     }
-
 }
